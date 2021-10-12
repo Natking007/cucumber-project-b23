b67dea568d63eb3c88fdb97b884bd17f78ddae56
Natking007 <77073455+Natking007@users.noreply.github.com>
2021-10-12 11:34:58 -0400

    Initial commit

---
+++ src/test/java/com/cydeo/step_definitions/DataTableStepDefs.java
@@ -0,0 +1,24 @@
+package com.cydeo.step_definitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class DataTableStepDefs {
+    @Given("I have a {string}")
+    public void i_have_a(String animal) {
+
+        System.out.println("Given I have a " + animal);
+
+    }
+    @When("I call their names")
+    public void i_call_their_names() {
+        System.out.println(" When I call their names ");
+    }
+    @Then("They come to me.")
+    public void they_come_to_me() {
+        System.out.println(" Then they come to me ");
+    }
+
+}
+
---
+++ src/test/resources/features/job.feature
@@ -0,0 +1,11 @@
+Feature: Getting jobs
+  Ad s student I should be able to het a job
+  Scenario: student prepare for a job
+  Given student attend all the classes
+  When student learn all the topics
+  Then student should be ready for the job
+
+  Scenario: student go to market
+    Given student is prepared
+    When student upload their resume
+    Then student will be in the marke
